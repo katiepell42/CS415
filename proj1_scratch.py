86e1da65431eb175e51ce5e8d775b177d2720f14
Katie Pell <kpell@blue.cs.sonoma.edu>
2021-09-26 15:27:48 -0700

    Project 1 final code

--- proj1_scratch.py
+++ proj1_scratch.py
@@ -1,9 +1,7 @@
 from random import randint
-from time import time
-from math import sqrt, floor
 
 # Problem 1 - Check if a number is prime
-# Solves x^y mod p using Figure 1.4 Modular Exponentiation
+# Solves x^y mod p
 def modular_expo(x, y, N):
     if y == 0:
         return 1
@@ -28,7 +26,7 @@ def primality2(N, k):
             return True
     return False
             
-#checks for prime number
+# Checks for prime number
 def primality3(N, k):
     checklist = [2, 3, 5, 7]
     for item in checklist:
@@ -53,38 +51,36 @@ def n_digit_prime(n, k):
 
 # Problem 3 - Encryption algorithm
 
-# Euclids Algo
-# def gcd(a, b):
-#     if b == 0:
-#         return a
-    
-#     return gcd(b, a%b)
+#Euclid's Extended Algorithim for GCD
+def gcd(a,b):
+    if b == 0:
+        return (1,0,a)
+    else:
+        x, y, extended_gcd = gcd(b, a % b)
 
-def encryption(n, k):
+        return y, x - (a//b)*y, extended_gcd
+
+def encryption(n, k, message):
     p = n_digit_prime(n, k)
     q = n_digit_prime(n, k)
     N = p * q
     S = (p - 1) * (q - 1)
     E = n_digit_prime(n, k)
-    lowest_divisor = gcd(E, S)
+    lowest_divisor = gcd(E, S)[2]
     while lowest_divisor != 1:
         E = n_digit_prime(n, k)
-        lowest_divisor = gcd(E, S)
+        lowest_divisor = gcd(E, S)[2]
 
     d = 1 / (E % S)
     print("\nN:", N)
     print("e:", E)
     print("d:", d)
 
-    message = int(input("enter a message: "))
     encrypted_message = modular_expo(message, E, N)
     decrypted_message = modular_expo(message, d, N)
 
-
-
     print("Encrypted message:", encrypted_message)
     print("Decrypted message: ", decrypted_message)
-
 
 # Problem 4 - Precise arithmetic operations
 def hsum(n):
@@ -110,34 +106,55 @@ def hsum2(n):
         j = j + 1
     return j
 
-
 # Part B
-def function4b(n):
+def g_series(n):
     if n == 0:
         return (1, 19)
     elif n == 1:
         return (3, 7)
 
-
-    # return 1.5 *(function4b(n - 1)[0]/function4b(n - 1)[1]) + function4b(n - 2)[0]/function4b(n - 2)[1]
-
-    numerator_first_term = 3 * function4b(n - 1)[0] * function4b(n - 2)[1]
-    numerator_second_term = 2 * function4b(n - 1)[1] * function4b(n - 2)[0]
+    numerator_first_term = 3 * g_series(n - 1)[0] * g_series(n - 2)[1]
+    numerator_second_term = 2 * g_series(n - 1)[1] * g_series(n - 2)[0]
     numerator = numerator_first_term + numerator_second_term
-    denominator = 2 * function4b(n - 1)[1] * function4b(n - 2)[1]
+    denominator = 2 * g_series(n - 1)[1] * g_series(n - 2)[1]
 
     return (numerator, denominator)
 
 
-    # (3/2)(a/b) + (c/d)
-    # 3a/2b + c/d
-    # (3ad + 2bc)/2bd
-
-
-
+def printMenu():
+    print("1. Solve problem 3")
+    print("2. Solve problem 4a")
+    print("3. Solve problem 4b")
+    print("4. Quit")
 
 def main():
-    # encryption(50,20)
-    print(function4b(2))
+    printMenu()
+    userInput = int(input("Enter an option from the menu: "))
+    while userInput != 4:
+        if userInput == 1:
+            n = int(input("Enter a value for n: "))
+            k = int(input("Enter a value for k: "))
+            M = int(input("Enter a message to encode: "))
+            encryption(n, k, M)
+        elif userInput == 2:
+            n = int(input("Enter a number: "))
+            print("It takes ", hsum2(n), " elements of the harmonic series in order for the sum to be greater than ", n, ".", sep="")
+        elif userInput == 3:
+            n = int(input("Enter a number: "))
+            if n % 10 == 1:
+                suffix = "st"
+            elif n % 10 == 2:
+                suffix = "nd"
+            elif n % 10 == 3:
+                suffix = "rd"
+            else:
+                suffix = "th"
+                
+            print("The ", n, suffix, " element of the G series is ", g_series(n), ".", sep="")
+        else:
+            userInput = int(input(("Please select a valid menu option: ")))
+
+        printMenu()
+        userInput = int(input("\nEnter another option from the menu: "))
 
 main()
